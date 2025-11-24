6caace7f19742e6153f60c515c55835587488e69
salm1ina <nabilaakbars1@gmail.com>
2025-11-24 10:08:52 +0800

    change

--- lib/page/choose_mood.dart
+++ lib/page/choose_mood.dart
@@ -19,17 +19,17 @@ class _ChooseMoodPageState extends State<ChooseMoodPage> {
     final List<Map<String, Object>> moods = [
       {
         "label": "Bad",
-        "color": Colors.redAccent,
+        "color": const Color.fromRGBO(255, 230, 230, 1),
         "image": "assets/images/bad.png",
       },
       {
         "label": "Fine",
-        "color": Colors.teal,
+        "color": const Color.fromRGBO(230, 255, 253, 1),
         "image": "assets/images/fine.png",
       },
       {
         "label": "Wonderful",
-        "color": Colors.amber,
+        "color": const Color.fromRGBO(255, 250, 230, 1),
         "image": "assets/images/wonderful.png",
       },
     ];
