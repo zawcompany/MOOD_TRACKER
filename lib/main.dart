f3a619f0b4ca6e1316b491f174978ed1161da1fc
zawcompany <151438772+zawcompany@users.noreply.github.com>
2025-12-01 06:10:07 +0800

    feat: Inisialisasi Firebase dan perbaikan pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // Provider
 import 'page/dashboard/presentation/provider/dashboard_provider.dart';
