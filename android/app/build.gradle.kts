2581e6824872a44fb9d1bd6e36e9b10298863c5c
salm1ina <nabilaakbars1@gmail.com>
2025-12-10 11:29:49 +0800

    fix(android): Resolve persistent NDK build failure by hardcoding version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 android {
     namespace = "com.example.mood_tracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+    ndkVersion = "27.0.12077973" 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-        // LOKASI BARU UNTUK DESUGARING
         isCoreLibraryDesugaringEnabled = true 
     }
 
@@ -25,10 +25,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.mood_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -38,8 +35,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -50,6 +45,5 @@ flutter {
 }
 
 dependencies {
-    // Dependency Desugaring
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
